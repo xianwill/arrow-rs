a46f84117132bdcc2682e30137f1685ccf52c1d6
xianwill <christianwilliams79@gmail.com>
2021-07-09 14:19:29 -0400

    Add make_row to public api

--- parquet/src/record/mod.rs
+++ parquet/src/record/mod.rs
@@ -23,6 +23,6 @@ mod record_writer;
 mod triplet;
 
 pub use self::{
-    api::{Field, List, ListAccessor, Map, MapAccessor, Row, RowAccessor},
+    api::{make_row, Field, List, ListAccessor, Map, MapAccessor, Row, RowAccessor},
     record_writer::RecordWriter,
 };
